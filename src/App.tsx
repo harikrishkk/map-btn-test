742f9825b65a88fcf0cfe9d37bbac1b760b7aace
Hari <harikrishkk@gmail.com>
2023-02-09 19:12:28 -0500

    app side

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Map } from '@harikrishkk/map-btn';
+import { TileLayer } from 'react-leaflet';
+import type { LatLngBoundsExpression } from 'leaflet';
 
-function App() {
+import 'leaflet/dist/leaflet.css';
+
+const App = () => {
+  const args = {
+    label: 'Default button',
+    style: {
+      padding: '20px',
+      width: '20em',
+      margin: '10px',
+      color: 'lightblue',
+      fontSize: '19px',
+      cursor: 'pointer',
+      border: '1px solid darkblue',
+      background: 'darkblue',
+    },
+    onClick: () => {
+      console.log('You clicked the Default button');
+    },
+  };
+
+  const bounds: LatLngBoundsExpression = [
+    [42.5993718217880613, 1.5937492475355806],
+    [45.9312500000000341, 7.6656250000000341],
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button label="My Library" style={args.style} onClick={args.onClick} />
+      </div>
+
+      <div>
+        <Map width="100vw" bounds={bounds}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </Map>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
